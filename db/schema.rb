c274e7dbdc69dfac9cdd55f3fc2cd4bb587401fb
Olivia Cubela <oliviacubela@gmail.com>
2020-09-10 22:39:04 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 5) do
+ActiveRecord::Schema.define(version: 6) do
+
+  create_table "actors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+  end
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
