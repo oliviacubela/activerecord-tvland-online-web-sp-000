bb01b11fe487a83ec4dcb2c4df210340b783a569
Olivia Cubela <oliviacubela@gmail.com>
2020-09-10 23:00:40 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,7 +2,7 @@ class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
 
-  def catchphrase
-    "#{catchphrase}"
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
   end
 end
