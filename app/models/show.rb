db407889ba1dcceca83823751db3bd1d57c9b515
Olivia Cubela <oliviacubela@gmail.com>
2020-09-10 22:53:39 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,6 +4,7 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
 
   def actors_list
-    self.actors.map{|actor| actor.full_name}
+    self.actors.map do |actor|
+      actor.full_name
   end
 end
